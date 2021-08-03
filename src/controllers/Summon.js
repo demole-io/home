c682716f187c3444a60b4e58ba73f5ac7577f4f9
Violet Hair <violethair2308@gmail.com>
2021-08-03 16:06:14 +0700

    update demo open egg on Summon

--- src/controllers/Summon.js
+++ src/controllers/Summon.js
@@ -1,69 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Header from '../components/Header'
 import PayBtn from '../assets/img/fantastic-illustration-ancient-chinese-themes 3.png'
-import Ques from '../assets/img/Ouf7Ajd 2.png'
 import Dragon from '../assets/img/rong 2.png'
-import Egg from '../assets/img/mockup 1.png'
-import $ from 'jquery'
+import EggOpen from '../assets/video/egg-open.mp4'
 
 export default function Summon() {
 
-    const reset = () => {
-        $(".egg").css({
-            opacity: 1,
-            display: 'block'
+    const video = useRef(null)
+    const dragon = useRef(null)
+    const summonSection = useRef(null)
+
+    useEffect(() => {
+
+        const summonSectionHeight = summonSection.current.offsetHeight
+
+        console.log(summonSectionHeight);
+
+        video.current.addEventListener("loadeddata", () => {
+            const videoHeight = video.current.videoHeight
+            let eggPositionAtBottom = videoHeight - (videoHeight * 0.72)
+            eggPositionAtBottom = summonSectionHeight - videoHeight + eggPositionAtBottom
+            dragon.current.style.bottom = `${eggPositionAtBottom}px`;
         })
 
-        $(".dragon").css({
-            opacity: 0,
-            display: 'none'
-        })
-
-    }
+        return () => {
+        };
+    }, []);
 
     const onClickPay = () => {
-
-        reset()
-        $(".egg").css({
-            opacity: 0,
-            transition: "opacity 2s",
-        })
-
+        video.current.play()
+        dragon.current.style["z-index"] = 1
         setTimeout(() => {
-
-            $(".egg").css({
-                display: 'none'
-            })
-
-            $(".dragon").css({
-                display: 'block',
-            })
-            setTimeout(() => {
-                $(".dragon").css({
-                    opacity: 1,
-                    transition: "opacity 2s",
-                })
-            }, 1000);
-        }, 1000);
+            dragon.current.style.opacity = 1
+        }, 1200)
     }
 
     return (
-        <div id="summon">
-            <Header></Header>
-            <div className="container">
-                <div className="waper">
-                    <div className="group1">
-                        <img className="egg" src={Egg} alt="photos"></img>
-                        <img className="dragon" src={Dragon} alt="photos"></img>
+        <div id="summon" ref={summonSection}>
+            <div className="video-background">
+                <video preload="auto" ref={video} src={EggOpen} muted={true} type="video/mp4"></video>
+            </div>
+            <img ref={dragon} className="dragon" src={Dragon} alt="photos"></img>
+            <div className="wrapper">
+                <Header></Header>
+                <div className="container">
+                    <div className="waper">
+                        <div className="group2">
+                            <img src={PayBtn} alt="photos" onClick={onClickPay}></img>
+                            {/* <img src={Ques} alt="photos" className="ques"></img> */}
+                        </div>
                     </div>
-
-                    <div className="group2">
-                        <img src={PayBtn} alt="photos" onClick={onClickPay}></img>
-                        <img src={Ques} alt="photos" className="ques"></img>
-                    </div>
-
                 </div>
-
             </div>
 
         </div>
