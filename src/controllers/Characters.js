1f36e7c14bfc664a1712a8caef0c036758ff439f
VanVan <s.sieuux@gmail.com>
2021-08-07 15:55:31 +0700

    update

--- src/controllers/Characters.js
+++ src/controllers/Characters.js
@@ -5,42 +5,41 @@ import ORC from '../assets/img/ezgif.com-gif-maker (7).webm'
 import WARRIOR from '../assets/img/ezgif.com-gif-maker (10).webm'
 import MEMALD from '../assets/img/ezgif.com-gif-maker (8).webm'
 import SHADOW from '../assets/webm/idealBoss-1.webm'
+import GOBLIN from '../assets/webm/Goblin_idle.webm'
+import HOBBIT from '../assets/webm/Hobbit.webm'
+import WOLF from '../assets/webm/ideal_nguoiSoi.webm'
+import COW from '../assets/webm/NguoiDauBo.webm'
 
 import iconOrc from '../assets/img/nguoi_cay.png'
 import iconMEMALD from '../assets/img/nguoi_ca.png'
 import iconKnight from '../assets/img/rong12.png'
 import iconWARRIOR from '../assets/img/thien_than.png'
 import iconSHADOW from '../assets/img/bong_dem.png'
+import iconGOBLIN from '../assets/img/phap su.png'
+import iconCOW from '../assets/img/nguoi xuong.png'
+import iconHobbit from '../assets/img/nguoi lun.png'
+import iconWolf from '../assets/img/nguoi soi.png'
+
 
 import OrcPoster from '../assets/img/Orc2.png'
 import TiencaPoster from '../assets/img/nguoica.png'
 import RongPoster from '../assets/img/rong 2.png'
 import TiennuPoster from '../assets/img/Tiennu.png'
 import ShadowPoster from '../assets/img/ezgif.com-gif-maker_11_-removebg-preview.png'
+import GoblinPoster from '../assets/webm/Goblin_idle-removebg-preview.png'
+import HobbitPoster from '../assets/webm/Hobbit-removebg-preview.png'
+import WolfPoster from '../assets/webm/ideal_nguoiSoi-removebg-preview.png'
+import CowPoster from '../assets/webm/NguoiDauBo-removebg-preview.png'
 
 import ArrowLeft from '../assets/img/muiten 2.png'
 import ArrowRight from '../assets/img/muiten 1.png'
-
 
 import Char2 from '../assets/img/chien binh.png'
 import Char3 from '../assets/img/chim.png'
 import Char4 from '../assets/img/cung thủ.png'
 import Char5 from '../assets/img/kỵ sĩ.png'
 import Char6 from '../assets/img/nguoi khong lo.png'
-import Char7 from '../assets/img/nguoi lun.png'
-import Char8 from '../assets/img/nguoi soi.png'
-import Char9 from '../assets/img/nguoi xuong.png'
 import Char10 from '../assets/img/nhan ma.png'
-import Char11 from '../assets/img/phap su.png'
-// import Char12 from '../assets/img/Giáp Bạc.png'
-// import Char13 from '../assets/img/Alien.png'
-// import Char14 from '../assets/img/Khô lâu.png'
-// import Char15 from '../assets/img/Linh Hồn.png'
-
-
-
-
-import $ from 'jquery'
 
 const Characters = props => {
     const isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
@@ -52,32 +51,83 @@ const Characters = props => {
             name: 'Treeman Tribe',
             icon: iconOrc,
             img: ORC,
+            className: 'orc',
+            poster: OrcPoster,
+            marginTop: 50,
             des: 'The murky old forests at the foot of the high cliffs in the northern part of the continent are inhabited by the Tree Tribe. Their territory is almost impregnable because of the murky darkness that is very scary. The inhabitants of the tribe were born from the trunks of thousands of years old trees, so their bodies were firm and soft, hazardous in team battles.'
         },
         {
             name: 'Merman Tribe',
             icon: iconMEMALD,
             img: MEMALD,
+            className: 'tienca',
+            poster: TiencaPoster,
+            marginTop: -15,
             des: 'Conquering the entire ocean, the Mermen have long harbored ambitions to dominate the continent. Mastering the vast sea, the Mermaids are especially strong in underwater battles. They regularly sent troops by the rivers to raid Middle-earth and infiltrate the mazes to loot resources.'
         },
         {
             name: 'Dragon Tribe',
             icon: iconKnight,
             img: Knight,
+            className: 'rong',
+            poster: RongPoster,
+            marginTop: -30,
             des: 'Living in the caves on the high cliffs of the northern continent, the Dragon tribe is a mighty tribe with wings spread in the sky, a muscular body, and sharp claws. Dragons also have hard scales like armor to protect the body. With the advantage of flying high, the dragon tribe proved to be dominant in single hunting.'
         },
         {
             name: 'Angel Tribe',
             icon: iconWARRIOR,
             img: WARRIOR,
+            className: 'tiennu',
+            poster: TiennuPoster,
+            marginTop: -15,
             des: 'The remote Arctic place covered by snow all year round is the home of the Angel tribe. They were born from holy souls. When they grow up, the members of the Angel Tribe spread out everywhere, linking up with many different tribes to help them fight. The Angel Tribe is famous as a tribe that has no enemies.'
         },
         {
             name: 'Shadow Tribe',
             icon: iconSHADOW,
             img: SHADOW,
+            className: 'shadow',
+            poster: ShadowPoster,
+            marginTop: 50,
             des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
-        }
+        },
+        // {
+        //     name: 'HOBBIT Tribe',
+        //     icon: iconHobbit,
+        //     img: HOBBIT,
+        //     className: 'hobbit',
+        //     poster: HobbitPoster,
+        //     marginTop: -60,
+        //     des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        // },
+        // {
+        //     name: 'WOLF Tribe',
+        //     icon: iconWolf,
+        //     img: WOLF,
+        //     className: 'wolf',
+        //     poster: WolfPoster,
+        //     marginTop: -30,
+        //     des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        // },
+        // {
+        //     name: 'GOBLIN Tribe',
+        //     icon: iconGOBLIN,
+        //     img: GOBLIN,
+        //     className: 'goblin',
+        //     poster: GoblinPoster,
+        //     marginTop:10,
+        //     des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        // },
+        // {
+        //     name: 'COW Tribe',
+        //     icon: iconCOW,
+        //     img: COW,
+        //     className: 'cow',
+        //     poster: CowPoster,
+        //     marginTop: 100,
+        //     des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        // },
     ])
 
     const [dataa] = useState([
@@ -85,39 +135,82 @@ const Characters = props => {
             name: 'Treeman Tribe',
             icon: iconOrc,
             img: ORC,
+            className: 'orc',
+            poster: OrcPoster,
+            marginTop: 100,
             des: 'The murky old forests at the foot of the high cliffs in the northern part of the continent are inhabited by the Tree Tribe. Their territory is almost impregnable because of the murky darkness that is very scary. The inhabitants of the tribe were born from the trunks of thousands of years old trees, so their bodies were firm and soft, hazardous in team battles.'
         },
         {
             name: 'Merman Tribe',
             icon: iconMEMALD,
             img: MEMALD,
+            className: 'tienca',
+            poster: TiencaPoster,
+            marginTop: 10,
             des: 'Conquering the entire ocean, the Mermen have long harbored ambitions to dominate the continent. Mastering the vast sea, the Mermaids are especially strong in underwater battles. They regularly sent troops by the rivers to raid Middle-earth and infiltrate the mazes to loot resources.'
         },
         {
             name: 'Dragon Tribe',
             icon: iconKnight,
             img: Knight,
+            className: 'rong',
+            poster: RongPoster,
+            marginTop: -30,
             des: 'Living in the caves on the high cliffs of the northern continent, the Dragon tribe is a mighty tribe with wings spread in the sky, a muscular body, and sharp claws. Dragons also have hard scales like armor to protect the body. With the advantage of flying high, the dragon tribe proved to be dominant in single hunting.'
         },
         {
             name: 'Angel Tribe',
             icon: iconWARRIOR,
             img: WARRIOR,
+            className: 'tiennu',
+            poster: TiennuPoster,
+            marginTop: -60,
             des: 'The remote Arctic place covered by snow all year round is the home of the Angel tribe. They were born from holy souls. When they grow up, the members of the Angel Tribe spread out everywhere, linking up with many different tribes to help them fight. The Angel Tribe is famous as a tribe that has no enemies.'
         },
         {
             name: 'Shadow Tribe',
             icon: iconSHADOW,
             img: SHADOW,
+            className: 'shadow',
+            poster: ShadowPoster,
+            marginTop: -80,
             des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
         },
-        // {
-        //     icon: Char1
-        // },
+        {
+            icon: iconHobbit,
+            img: HOBBIT,
+            className: 'hobbit',
+            poster: HobbitPoster,
+            marginTop: -60,
+            des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        },
+        {
+            icon: iconWolf,
+            img: WOLF,
+            className: 'wolf',
+            poster: WolfPoster,
+            marginTop: -30,
+            des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        },
+        {
+            icon: iconGOBLIN,
+            img: GOBLIN,
+            className: 'goblin',
+            poster: GoblinPoster,
+            marginTop:10,
+            des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        },
+        {
+            icon: iconCOW,
+            img: COW,
+            className: 'cow',
+            poster: CowPoster,
+            marginTop: 100,
+            des: 'The shadow tribe is not of the world. Coming from the center of the galaxy, Shadow warriors are created by dark energy, extremely cruel and bloodthirsty. This hideously terrifying Shadow warrior desperately seeks for a world to live long in and to harbor ambitions for domination.'
+        },
         {
             icon: Char2
         },
-
         {
             icon: Char3
         },
@@ -131,78 +224,13 @@ const Characters = props => {
             icon: Char6
         },
         {
-            icon: Char7
-        },
-        {
-            icon: Char8
-        },
-        {
-            icon: Char9
-        },
-        {
             icon: Char10
         },
-        {
-            icon: Char11
-        },
-        // {
-        //     icon: Char12
-        // },
-        // {
-        //     icon: Char13
-        // },
-        // {
-        //     icon: Char14
-        // },
-        // {
-        //     icon: Char15
-        // }
     ])
 
     const selected = useRef(0)
     const [index, setindex] = useState(0)
     const interval = useRef(null)
-
-    const handleScroll = () => {
-        const pageCharacters = document.querySelector("#characters")
-        $('#characters .container .des .txtt').addClass('text-zoom')
-        $('#characters .container .group2-mobie .txtt').addClass('text-zoom')
-
-        if (pageCharacters.getBoundingClientRect().top > 0) {
-            $('#characters .container .des .txtt').removeClass('text-zoom')
-            $('#characters .container .group2-mobie .txtt').removeClass('text-zoom')
-        }
-        //BOTTOM
-        if (pageCharacters.getBoundingClientRect().bottom <= 0) {
-            $('#characters .container .des .txtt').removeClass('text-zoom')
-            $('#characters .container .group2-mobie .txtt').removeClass('text-zoom')
-        }
-    }
-
-    useEffect(() => {
-        interval.current = setInterval(() => {
-            selected.current = selected.current === data.length - 1 ? 0 : selected.current + 1;
-            setindex(selected.current)
-            $('#characters .container .des .txtt').addClass('text-zoom')
-            $('#characters .container .group2-mobie .txtt').addClass('text-zoom')
-
-            setTimeout(() => {
-                $('#characters .container .des .txtt').removeClass('text-zoom')
-                $('#characters .container .group2-mobie .txtt').removeClass('text-zoom')
-            }, 2000);
-
-        }, 10000)
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            if (interval.current) clearInterval(interval.current)
-
-            window.removeEventListener('scroll', handleScroll);
-            $('#characters .container .des .txtt').removeClass('text-zoom')
-            $('#characters .container .group2-mobie .txtt').removeClass('text-zoom')
-        }
-    }, [data]);
 
     const stopNext = () => {
         if (interval.current) clearInterval(interval.current)
@@ -245,11 +273,9 @@ const Characters = props => {
             <div className="container">
                 <div className="wraper-name-mobile">
                     <div>
-                        <img onClick={() => onClickName(0)} className={`${index === 0 ? 'selected' : ''}`} style={{ marginTop: '50px' }} src={data[0].icon} alt="photos"></img>
-                        <img onClick={() => onClickName(1)} className={`${index === 1 ? 'selected' : ''}`} style={{ marginTop: '-15px' }} src={data[1].icon} alt="photos"></img>
-                        <img onClick={() => onClickName(2)} className={`${index === 2 ? 'selected' : ''}`} style={{ marginTop: '-30px' }} src={data[2].icon} alt="photos"></img>
-                        <img onClick={() => onClickName(3)} className={`${index === 3 ? 'selected' : ''}`} style={{ marginTop: '-15px' }} src={data[3].icon} alt="photos"></img>
-                        <img onClick={() => onClickName(4)} className={`${index === 4 ? 'selected' : ''}`} style={{ marginTop: '50px' }} src={data[4].icon} alt="photos"></img>
+                        {data.map((value, indexx) => {
+                            return <img onClick={() => onClickName(indexx)} className={`${index === indexx ? 'selected' : ''}`} style={{ marginTop: `${value.marginTop}px` }} src={value.icon} alt="photos"></img>
+                        })}
                     </div>
 
                     <img className="hmm" src={Unknow1} alt="photos"></img>
@@ -258,19 +284,23 @@ const Characters = props => {
                     <div className="left">
                         <img className="arrow" src={ArrowLeft} onClick={() => onClickLeft()} alt="photos"></img>
                         {!isIOS && <div className="char">
-                            {index === 0 && <video muted={true} className="orc" autoPlay={true} loop={true} src={data[index].img} type="video/webm"></video>}
-                            {index === 1 && <video muted={true} className="tienca" autoPlay={true} loop={true} src={data[index].img} type="video/webm"></video>}
-                            {index === 2 && <video muted={true} className="rong" autoPlay={true} loop={true} src={data[index].img} type="video/webm"></video>}
-                            {index === 3 && <video muted={true} className="tiennu" autoPlay={true} loop={true} src={data[index].img} type="video/webm"></video>}
-                            {index === 4 && <video muted={true} className="shadow" autoPlay={true} loop={true} src={data[index].img} type="video/webm"></video>}
+                            {data.map((value, indexx) => {
+                                if (index === indexx) {
+                                    return <video muted={true} className={value.className} autoPlay={true} loop={true} src={value.img} type="video/webm"></video>
+                                }
+                            })}
                         </div>}
 
                         {isIOS && <div className="char">
-                            {index === 0 && <img className="orc" src={OrcPoster} alt="photos"></img>}
-                            {index === 1 && <img className="tienca" src={TiencaPoster} alt="photos"></img>}
-                            {index === 2 && <img className="rong" src={RongPoster} alt="photos"></img>}
-                            {index === 3 && <img className="tiennu-iso" src={TiennuPoster} alt="photos"></img>}
-                            {index === 4 && <img className="shadow" src={ShadowPoster} alt="photos"></img>}
+                            {data.map((value, indexx) => {
+                                if (index === indexx) {
+                                    if (indexx === 3) {
+                                        return <img className="tiennu-iso" src={value.poster} alt="photos"></img>
+                                    } else {
+                                        return <img className={value.className} src={value.poster} alt="photos"></img>
+                                    }
+                                }
+                            })}
                         </div>}
                         <img className="arrow" src={ArrowRight} onClick={() => onClickRight()} alt="photos"></img>
                     </div>
