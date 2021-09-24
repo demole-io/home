866805debd512f23bef780c5635063c0e4b3a09b
VanVan <s.sieuux@gmail.com>
2021-09-24 16:32:12 +0700

    update BackedBy

--- src/controllers/Backed.js
+++ src/controllers/Backed.js
@@ -1,42 +1,24 @@
-// import React, { useState } from 'react'
-import React from 'react'
-// import Bingbon from '../assets/img/bingbon1.png'
-// import Bitcoin from '../assets/img/NoPath.png'
-// import Okex from '../assets/img/OKEX1.png'
-// import Directid from '../assets/img/directid-logo.png'
-// import Drope from '../assets/img/dropel.png'
-// import Ftx from '../assets/img/ftx.png'
-// import Ccn from '../assets/img/NoPath - Copy (2).png'
-// import Huobi from '../assets/img/组件.png'
-// import Easypost from '../assets/img/easypost_logo.png'
-// import Bibox from '../assets/img/Group 8090.png'
-// import Binance from '../assets/img/NoPath - Copy (7).png'
-// import Monero from '../assets/img/NoPath - Copy (9).png'
+import React, { useState } from 'react'
+import Metrix from '../assets/img/logo-metrix.png'
+import Interchainventures from '../assets/img/logo-interchainventures.webp'
+import Lupa from '../assets/img/LupaX.png'
 
 export default function Backed() {
 
-    // const [data, setdata] = useState([
-    //     { img: Bingbon },
-    //     { img: Bitcoin },
-    //     { img: Okex },
-    //     { img: Directid },
-    //     { img: Drope },
-    //     { img: Ftx },
-    //     { img: Ccn },
-    //     { img: Huobi },
-    //     { img: Easypost },
-    //     { img: Bibox },
-    //     { img: Binance },
-    //     { img: Monero },
-    // ])
+    const [data, setdata] = useState([
+        { img: Metrix },
+        { img: Interchainventures },
+        { img: Lupa }
+
+    ])
     return (
         <div id="backed">
             <div className="container">
-            <p className="title">BACKED BY</p>
+                <p className="title">BACKED BY</p>
                 <div className="content">
-                    {/* {data.map((value, index) => {
+                    {data.map((value, index) => {
                         return <img src={value.img} alt="photos"></img>
-                    })} */}
+                    })}
                 </div>
 
                 <div className="sketchfab-embed-wrapper">
