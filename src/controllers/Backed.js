48341166975282157e3e34d9fce5b288740999ca
VanVan <s.sieuux@gmail.com>
2021-10-07 17:02:41 +0700

    update logo

--- src/controllers/Backed.js
+++ src/controllers/Backed.js
@@ -1,36 +1,48 @@
 import React, { useState } from 'react'
-import Metrix from '../assets/img/logo-metrix.png'
-import Interchainventures from '../assets/img/logo-interchainventures.png'
-import Lupa from '../assets/img/LupaX.png'
-import DaoMaker from '../assets/img/dao-maker.svg'
-import LDCapial from '../assets/img/ld-capital.svg'
-import Gate from '../assets/img/gate-logo.png'
+
+import AC from '../assets/LOGO/AC.svg'
+import BasicCapital from '../assets/LOGO/5496986_1620279486.png'
+import ConsensusLab from '../assets/LOGO/Top002070-logo1.png'
+import DaoMaker from '../assets/LOGO/dao-maker.svg'
+import Interchain from '../assets/LOGO/logo-interchainventures.png'
+import K24 from '../assets/LOGO/K24 Venture.png'
+import Kirin from '../assets/LOGO/Kirin.png'
+import LDCapital from '../assets/LOGO/ld-capital.svg'
+import logoX21 from '../assets/LOGO/x21-logo.png'
+import LupaX from '../assets/LOGO/LupaX.png'
+import Magnus from '../assets/LOGO/MagnusLogo-PSD_white.png'
+import MetrixCapital from '../assets/LOGO/logo-metrix.png'
+import Gate from '../assets/LOGO/gate-logo.png'
+import Raptor from '../assets/LOGO/logo.png'
+import Newave from '../assets/LOGO/Newave Capital logo1.svg'
 
 export default function Backed() {
-
-    const [data2, setdata2] = useState([
-        { img: DaoMaker },
-        { img: LDCapial },
-        { img: Gate }
-    ])
-
     const [data, setdata] = useState([
-        { img: Metrix },
-        { img: Interchainventures },
-        { img: Lupa }
+        { img: AC },
+        { img: BasicCapital },
+        { img: ConsensusLab },
+        { img: DaoMaker },
+        { img: Interchain },
+        { img: K24 },
+        { img: Kirin },
+        { img: LDCapital },
+        { img: logoX21 },
+        { img: LupaX },
+        { img: Magnus },
+        { img: MetrixCapital },
+        { img: Gate },
+        { img: Raptor },
+        { img: Newave },
     ])
     return (
         <div id="backed">
             <div className="container">
                 <p className="title">BACKED BY</p>
-                <div className="content" style={{ marginBottom: 50 }}>
-                    {data2.map((value, index) => {
-                        return <img src={value.img} width={250} alt="photos"></img>
-                    })}
-                </div>
                 <div className="content">
                     {data.map((value, index) => {
-                        return <img src={value.img} alt="photos"></img>
+                        return <div>
+                            <img src={value.img} alt="photos"></img>
+                        </div>
                     })}
                 </div>
 
