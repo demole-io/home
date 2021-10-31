d9aee9c33e0a70650c6a13ff34d3833e9c2ba537
MAC <hoangminh881997@gmail.com>
2021-10-31 21:47:54 +0700

    change-logo

--- src/controllers/Backed.js
+++ src/controllers/Backed.js
@@ -12,35 +12,40 @@ import logoX21 from '../assets/LOGO/x21-logo.png'
 import LupaX from '../assets/LOGO/LupaX.png'
 import Magnus from '../assets/LOGO/MagnusLogo-PSD_white.png'
 import MetrixCapital from '../assets/LOGO/logo-metrix.png'
-import Gate from '../assets/LOGO/gate-logo.png'
+import Gate from '../assets/LOGO/gate-io.png'
 import Raptor from '../assets/LOGO/logo.png'
 import Newave from '../assets/LOGO/Newave Capital logo1.svg'
-import Au21 from '../assets/LOGO/au21.png'
+import Au21 from '../assets/LOGO/Au21logo.png'
 import Capstonetrade from '../assets/LOGO/footer_logo.png'
-import M6 from '../assets/LOGO/1200px-M6_logo_2020.svg.png'
+import M6 from '../assets/LOGO/M6.png'
 import Evg from '../assets/LOGO/evg.svg'
 import Rikkei from '../assets/LOGO/77c4d5f.png'
+import GameStarter from '../assets/LOGO/Game-Starter-01.png'
+import UG from '../assets/LOGO/venture.png'
 
 export default function Backed() {
     const [data] = useState([
-        { img: AC },
-        { img: BasicCapital },
-        { img: ConsensusLab },
         { img: DaoMaker },
-        { img: Interchain },
-        { img: K24 },
-        { img: Kirin },
-        { img: LDCapital },
-        { img: logoX21 },
         { img: LupaX },
-        { img: Magnus },
         { img: MetrixCapital },
         { img: Gate },
+        { img: GameStarter },
+        { img: LDCapital },
+        { img: M6 },
+        { img: Magnus },
+        { img: AC },
+        { img: Au21 },
+        { img: logoX21 },
+        { img: BasicCapital },
+        { img: ConsensusLab },
+        { img: K24 },
+        { img: Kirin },
         { img: Raptor },
-        { img: Newave },
+        { img: UG },
+        // { img: Interchain },
+        // { img: Newave },
         // { img: Au21 },
         // { img: Capstonetrade },
-        // { img: M6 },
         // { img: Evg },
         // { img: Rikkei }
     ])
