acf19dcdb4eff824bbe4145a98fb1431b6e40242
demole-io <93251275+demole-io@users.noreply.github.com>
2021-11-29 14:21:09 +0700

    Update Backed.js

--- src/controllers/Backed.js
+++ src/controllers/Backed.js
@@ -61,7 +61,6 @@ export default function Backed() {
                 </div>
 
                 <div className="sketchfab-embed-wrapper">
-                    <iframe title="Demole.io Model 1" frameBorder="0" allowFullScreen allow="fullscreen; autoplay; vr" xr-spatial-tracking="1" execution-while-out-of-viewport="1" execution-while-not-rendered="1" web-share="1" src="https://sketchfab.com/models/6732e15989d5429f9af50bcf01448f48/embed?autostart=1&transparent=1&ui_animations=0&ui_infos=0&ui_stop=0&ui_inspector=0&ui_help=0&ui_settings=0&ui_vr=0&ui_fullscreen=0&ui_annotations=0&ui_theme=dark"> </iframe>
                 </div>
             </div>
         </div>
